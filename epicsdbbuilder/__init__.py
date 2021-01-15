5cbf9f66ea47b596071bf1d857aff2b7d62e402e
Ivo Hanak <hanak@ipp.cas.cz>
2021-01-15 17:49:43 +0100

    Updated according to code reviewer comments
     1) Broke a line over 80 chars at __init__.py

--- epicsdbbuilder/__init__.py
+++ epicsdbbuilder/__init__.py
@@ -17,4 +17,5 @@ def ExportModules(globs, base_module, modulenames):
 
 __all__ = ExportModules(globals(),
     'epicsdbbuilder',
-    ['dbd', 'recordbase', 'fanout', 'recordset', 'recordnames', 'parameter', 'const_array'])
+    ['dbd', 'recordbase', 'fanout', 'recordset', 'recordnames', 'parameter',
+        'const_array'])
