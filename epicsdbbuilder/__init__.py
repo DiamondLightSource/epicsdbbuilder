3b4af789dc44d46ff827c70046ff1e1a56ca476b
Tom Cobb <tom.cobb@diamond.ac.uk>
2021-02-22 13:15:15 +0000

    Build system changes

--- epicsdbbuilder/__init__.py
+++ epicsdbbuilder/__init__.py
@@ -1,21 +1,11 @@
 # EPICS Database Building Framework
 
-# Helper function to export all named modules
-def ExportModules(globs, base_module, modulenames):
-    names = []
-    for modulename in modulenames:
-        # This code for importing the module could be simplified by using
-        # importlib.import_module(), but that's only available for Python 2.7
-        module = __import__(
-            '%s.%s' % (base_module, modulename),
-            globals(), locals(), [modulename], 0)
-
-        for name in module.__all__:
-            globs[name] = getattr(module, name)
-        names.extend(module.__all__)
-    return names
-
-__all__ = ExportModules(globals(),
-    'epicsdbbuilder',
-    ['dbd', 'recordbase', 'fanout', 'recordset', 'recordnames', 'parameter',
-        'const_array'])
+# All these have an __all__ so rely on that
+from epicsdbbuilder.dbd import *
+from epicsdbbuilder.recordbase import *
+from epicsdbbuilder.fanout import *
+from epicsdbbuilder.recordset import *
+from epicsdbbuilder.recordnames import *
+from epicsdbbuilder.parameter import *
+from epicsdbbuilder.const_array import *
+from epicsdbbuilder._version_git import __version__
