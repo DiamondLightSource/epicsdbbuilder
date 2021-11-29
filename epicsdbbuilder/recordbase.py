30d3f48720021d6209a80e9a6f5aea6f4304d754
Tom Cobb <tom.cobb@diamond.ac.uk>
2021-11-29 13:38:13 +0000

    Make alphabetical sort of records and fields optional (#25)
    * Make alphabetical sort of records and fields optional
    
    * Review comments

--- epicsdbbuilder/recordbase.py
+++ epicsdbbuilder/recordbase.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 import string
 import json
+from collections import OrderedDict
 
 from . import recordnames
 from .recordset import recordset
@@ -112,8 +113,8 @@ class Record(object):
 
         # These assignment have to be directly into the dictionary to
         # bypass the tricksy use of __setattr__.
-        self.__setattr('__fields', {})
-        self.__setattr('__aliases', set())
+        self.__setattr('__fields', OrderedDict())
+        self.__setattr('__aliases', OrderedDict())
         self.__setattr('__comments', [])
         self.__setattr('__infos', [])
         self.__setattr('name', recordnames.RecordName(record))
@@ -132,9 +133,8 @@ class Record(object):
 
         recordset.PublishRecord(self.name, self)
 
-
     def add_alias(self, alias):
-        self.__aliases.add(alias)
+        self.__aliases[alias] = self
 
     def add_comment(self, comment):
         self.__comments.append('# ' + comment)
@@ -145,10 +145,18 @@ class Record(object):
     def add_info(self, name, info):
         self.__infos.append((name, info))
 
+    def __dbd_order(self, fields):
+        field_set = set(fields)
+        for field_name in self._validate.dbEntry.iterate_fields():
+            if field_name in field_set:
+                yield field_name
+                field_set.remove(field_name)
+        assert not field_set, "DBD for %s doesn't contain %s" % (
+            self._type, sorted(field_set))
 
     # Call to generate database description of this record.  Outputs record
     # definition in .db file format.  Hooks for meta-data can go here.
-    def Print(self, output):
+    def Print(self, output, alphabetical=True):
         print(file = output)
         for comment in self.__comments:
             print(comment, file=output)
@@ -157,14 +165,16 @@ class Record(object):
         # Print the fields in alphabetical order.  This is more convenient
         # to the eye and has the useful side effect of bypassing a bug
         # where DTYPE needs to be specified before INP or OUT fields.
-        for k in sorted(self.__fields.keys()):
+        sort = sorted if alphabetical else self.__dbd_order
+        for k in sort(self.__fields.keys()):
             value = self.__fields[k]
             if getattr(value, 'ValidateLater', False):
                 self.__ValidateField(k, value)
             value = self.__FormatFieldForDb(k, value)
             padding = ''.ljust(4-len(k))  # To align field values
             print('    field(%s, %s%s)' % (k, padding, value), file = output)
-        for alias in sorted(list(self.__aliases)):
+        sort = sorted if alphabetical else list
+        for alias in sort(self.__aliases.keys()):
             print('    alias("%s")' % alias, file = output)
         for name, info in self.__infos:
             value = self.__FormatFieldForDb(name, info)
